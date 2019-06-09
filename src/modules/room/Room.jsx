c2fe5f7ca515ad8f1e572e59faecd2be90bdc5d9
idanlo <idan2110@gmail.com>
2019-06-09 12:22:35 +0300

    change to class component which makes it much easier to keep event listeners in sync

--- src/modules/room/Room.jsx
+++ src/modules/room/Room.jsx
@@ -3,191 +3,183 @@ import Youtube from 'react-youtube';
 import axios from 'axios';
 import { database } from '../../firebase';
 
-function Room(props) {
-  const [roomId, setRoomId] = React.useState('');
-  const [userId, setUserId] = React.useState('');
-  const [videoId, setVideoId] = React.useState('');
-  const [player, setPlayer] = React.useState(null);
-  const [query, setQuery] = React.useState('');
-  const [state, setState] = React.useState('');
-  const [searchResults, setSearchResults] = React.useState(null);
+const mapPlayerStateNumberToValue = {
+  0: 'ended',
+  1: 'playing',
+  2: 'paused',
+  3: 'buffering',
+  5: 'video cued'
+};
 
-  // set room id
-  React.useEffect(() => {
-    setRoomId(props.match.params.id);
-  }, [props.match.params.id]);
+class Room extends React.Component {
+  state = {
+    roomId: '',
+    userId: '',
+    videoId: '',
+    query: '',
+    state: '',
+    searchResults: null
+  };
+  player = null;
 
-  // set user id
-  React.useEffect(() => {
-    if (roomId.length) {
-      // get available key for a new user
-      const userKey = database
-        .ref('rooms/' + roomId)
-        .child('users')
-        .push().key;
+  componentDidMount() {
+    // add window event listener for leaving the page (leave room)
+    window.addEventListener('beforeunload', this.beforeUnloadListener);
+    // get room id from url path params
+    const roomId = this.props.match.params.id;
+    // get available key for a new user in the room
+    const userId = database
+      .ref('rooms/' + roomId)
+      .child('users')
+      .push().key;
 
-      setUserId(userKey);
+    // create the user in the database
+    database.ref('rooms/' + roomId + '/users/' + userId).set(true);
+    // listen to value changes in the room
+    database.ref('rooms/' + roomId).on('value', snap => {
+      let videoId;
 
-      database.ref('rooms/' + roomId + '/users/' + userKey).set(true);
-    }
-  }, [roomId]);
+      if (snap.child('video').val()) {
+        // if video id already exists in room then obtain it
+        videoId = snap.child('video').val();
+      } else if (snap.exists()) {
+        // if video id doesn't exist then use this default one
+        snap.child('video').ref.set('2Vv-BfVoq4g');
+        videoId = '2Vv-BfVoq4g';
+      }
+      this.setState({ videoId });
 
-  // get current playing video, if there are no playing video then play one
-  React.useEffect(() => {
-    if (userId && roomId) {
-      database.ref('rooms/' + roomId).on('value', snap => {
-        if (snap.child('video').val()) {
-          // if video id already exists in room then obtain it
-          setVideoId(snap.child('video').val());
-        } else if (snap.exists()) {
-          // if video id doesn't exist then use this default one
-          snap.child('video').ref.set('2Vv-BfVoq4g');
+      if (this.player) {
+        if (
+          Math.abs(snap.child('time').val() - this.player.getCurrentTime()) >
+          0.5
+        ) {
+          this.player.seekTo(snap.child('time').val());
         }
-      });
-    }
-    return () => database.ref('rooms/' + roomId).off('value');
-  }, [roomId, userId]);
 
-  // get current playing video, if there are no playing video then play one
-  React.useEffect(() => {
-    if (player && userId && roomId) {
-      database.ref('rooms/' + roomId + '/time').on('value', snap => {
-        console.log('ONME');
-        if (snap.val() !== player.getCurrentTime()) {
-          player.seekTo(snap.val());
-        }
-      });
-      database.ref('rooms/' + roomId + '/state').on('value', snap => {
-        if (snap.val() !== state) {
-          {
-            if (snap.val() === 'paused') {
-              player.pauseVideo();
-            } else if (snap.val() === 'playing') {
-              player.playVideo();
-            }
+        const newState = snap.child('state').val();
+        if (
+          newState !== mapPlayerStateNumberToValue[this.player.getPlayerState]
+        ) {
+          if (newState === 'paused') {
+            this.player.pauseVideo();
+          } else if (newState === 'playing') {
+            this.player.playVideo();
           }
         }
-      });
-    }
-    return () => database.ref('rooms/' + roomId + '/time').off('value');
-  }, [player, roomId, state, userId]);
+      }
+    });
 
-  // leave room when window closes
-  // TODO: when user goes back in chrome but doesn't leave the page then it doesn't remove him from the user list
-  React.useEffect(() => {
-    const handler = () => {
-      // handle use leave room event
-      database.ref('rooms/' + roomId + '/users/' + userId).remove();
-      // if there are no other users - delete the room
-      database
-        .ref('rooms/' + roomId + '/users')
-        .once('value')
-        .then(data => {
-          if (!data.val()) {
-            database.ref('rooms/' + roomId).remove();
-          }
-        });
-    };
-
-    window.addEventListener('beforeunload', handler);
-    return () => window.removeEventListener('beforeunload', handler);
-  }, [roomId, userId]);
-
-  function onReady(e) {
-    setPlayer(e.target);
-    console.log(e);
+    this.setState({ roomId, userId });
   }
 
-  function onStateChange(e) {
-    let newState = 'unstarted'; // this is when e.data === -1.
-    let time = 0;
-    if (e.data === 0) {
-      newState = 'ended';
-    } else if (e.data === 1) {
-      newState = 'playing';
-    } else if (e.data === 2) {
-      newState = 'paused';
-    } else if (e.data === 3) {
-      newState = 'buffering';
-    } else if (e.data === 5) {
-      newState = 'video cued';
-    }
-    setState(newState);
-    time = e.target.getCurrentTime();
-    console.log(newState, time);
+  componentWillUnmount() {
+    // remove window event listener on unmount
+    window.removeEventListener('beforeunload', this.beforeUnloadListener);
+    // remove event listener for changed values in the room
+    database.ref('rooms/' + this.state.roomId).off('value');
+  }
+
+  beforeUnloadListener = () => {
+    const { userId, roomId } = this.state;
+    // handle use leave room event
+    database.ref('rooms/' + roomId + '/users/' + userId).remove();
+    // if there are no other users - delete the room
     database
-      .ref('rooms/' + roomId + '/time')
+      .ref('rooms/' + roomId + '/users')
       .once('value')
-      .then(snap => {
-        if (Math.abs(snap.val() - time) > 0.75) {
-          database.ref('rooms/' + roomId).update({
-            time
-          });
+      .then(data => {
+        if (!data.val()) {
+          database.ref('rooms/' + roomId).remove();
         }
       });
+  };
+
+  onReady = e => {
+    this.player = e.target;
+    console.log('Player connected - ', e);
+  };
+
+  onStateChange = e => {
+    let time = 0;
+
+    let newState = mapPlayerStateNumberToValue[e.data];
+
+    if (!newState) {
+      newState = 'unstarted';
+    }
+    console.log('NEWSTATE', newState);
+    time = this.player.getCurrentTime();
+    console.log('State changed ', newState, time);
     if (newState !== 'buffering') {
-      database.ref('rooms/' + roomId).update({
+      database.ref('rooms/' + this.state.roomId).update({
+        time,
         state: newState
       });
     }
-  }
+  };
 
-  function Search() {
+  Search = () => {
     axios
       .get('https://www.googleapis.com/youtube/v3/search', {
         params: {
           key: process.env.REACT_APP_YOUTUBE_API_KEY,
           maxResults: '25',
           part: 'snippet',
-          q: query,
+          q: this.state.query,
           type: 'video'
         }
       })
       .then(res => {
         console.log(res.data);
-        setSearchResults(res.data.items);
+        this.setState({ searchResults: res.data.items });
       });
-  }
-
-  function changeVideo(newVideoId) {
-    database.ref('rooms/' + roomId + '/video/').set(newVideoId);
-  }
-
-  const options = {
-    width: '720',
-    height: '480',
-    // width: '100%',
-    // height: '100%',
-    playerVars: {
-      autoplay: 1
-    }
   };
 
-  return (
-    <div>
-      <h1>Room</h1>
-      <Youtube
-        opts={options}
-        onReady={onReady}
-        videoId={videoId}
-        onStateChange={onStateChange}
-      />
+  changeVideo = newVideoId => {
+    database.ref('rooms/' + this.state.roomId + '/video').set(newVideoId);
+  };
+
+  render() {
+    const options = {
+      width: '720',
+      height: '480',
+      // width: '100%',
+      // height: '100%',
+      playerVars: {
+        autoplay: 1
+      }
+    };
+
+    return (
       <div>
-        <input type="text" onChange={e => setQuery(e.target.value)} />
-        <button onClick={Search}>Search</button>
-        {searchResults
-          ? searchResults.map(result => (
-              <div
-                key={result.id.videoId}
-                onClick={() => changeVideo(result.id.videoId)}
-              >
-                <h1>{result.snippet.title}</h1>
-              </div>
-            ))
-          : null}
+        <h1>Room</h1>
+        <Youtube
+          opts={options}
+          onReady={this.onReady}
+          videoId={this.state.videoId}
+          onStateChange={this.onStateChange}
+        />
+        <div>
+          <input
+            type="text"
+            onChange={e => this.setState({ query: e.target.value })}
+          />
+          <button onClick={this.Search}>Search</button>
+          {this.state.searchResults
+            ? this.state.searchResults.map(result => (
+                <div
+                  key={result.id.videoId}
+                  onClick={() => this.changeVideo(result.id.videoId)}
+                >
+                  <h1>{result.snippet.title}</h1>
+                </div>
+              ))
+            : null}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Room;
